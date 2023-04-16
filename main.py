27adf12c116584faf6a15b25712a3dbc2c63bd2c
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-04-16 22:43:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+
+def start_download():
+    try:
+        yt_link = link.get()
+        yt_object = YouTube(yt_link, on_progress_callback=on_progress)
+        video = yt_object.streams.get_highest_resolution()
+        # ytObject.streams
+
+        title.configure(text=yt_object.title, text_color="white")
+        finishLabel.configure(text="")
+        video.download()
+        finishLabel.configure(text="Downloaded!")
+    except:
+        finishLabel.configure(text="Downloaded Error", text_color="red")
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_compeletion = bytes_downloaded / total_size * 100
+    per = str(int(percentage_of_compeletion))
+    pPercentage.configure(text=per + '%')
+    pPercentage.update()
+
+    # Update progress bar
+    progressBar.set(float(percentage_of_compeletion) / 100)
+
+
+# System Setting
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("blue")
+
+# Our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("YouTube Downloader")
+
+# Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=370, height=40, textvariable=url_var)
+link.pack()
+
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress percentage
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
+# Download Button
+download = customtkinter.CTkButton(app, text="Download", command=start_download)
+download.pack(padx=10, pady=10)
+
+# Run app
+app.mainloop()
